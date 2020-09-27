23478bc6e5b62649bfedaa184bda599754eec7cc
ryantsui1109 <68107903+ryantsui1109@users.noreply.github.com>
2020-09-27 11:41:02 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -8,24 +8,22 @@ $(document).ready(function() {
         var g = getRandom()
         var b = getRandom()
         $('body').css('background-color', `rgb(${r},${g},${b})`);
-        if (r + g + b >= 383) {
-            $('h1').css('color', 'black')
-        }
-        else {
-            $('h1').css('color', 'white')
+        if (r + g + b > 382) {
+            $('h1,h3').css('color', 'black')
+        } else {
+            $('h1,h3').css('color', 'white')
         }
     }
 
     function updateText() {
         $('h1,title').text(prompt(`請輸入顯示訊息`));
     }
-    $('body').keydown(function(e) {
-        if (e.keyCode == 13) {
+
+    $('body').keydown(function(key) {
+        if (key.keyCode == 13) {
             updateText()
         }
     });
-
-
 
     updateText()
     setInterval(() => {
